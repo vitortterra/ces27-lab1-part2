98335d756d26d38f1d45a84d1f31586a78fff0a1
vitortterra <vitortterra@gmail.com>
2016-09-18 11:20:49 -0300

    Lab 1 - Part 2 Solution
    Implement fault tolerance in distributed mode

---
+++ master.go
@@ -0,0 +1,100 @@
+package mapreduce
+
+import (
+	"log"
+	"net"
+	"net/rpc"
+	"sync"
+)
+
+const (
+	IDLE_WORKER_BUFFER     = 100
+	RETRY_OPERATION_BUFFER = 100
+)
+
+type Master struct {
+	// Task
+	task *Task
+
+	// Network
+	address   string
+	rpcServer *rpc.Server
+	listener  net.Listener
+
+	// Workers handling
+	workersMutex sync.Mutex
+	workers      map[int]*RemoteWorker
+	totalWorkers int // Used to generate unique ids for new workers
+
+	idleWorkerChan   chan *RemoteWorker
+	failedWorkerChan chan *RemoteWorker
+
+	///////////////////////////////
+	// ADD EXTRA PROPERTIES HERE //
+	///////////////////////////////
+	// Fault Tolerance
+	failedOperation chan bool
+}
+
+type Operation struct {
+	proc     string
+	id       int
+	filePath string
+}
+
+// Construct a new Master struct
+func newMaster(address string) (master *Master) {
+	master = new(Master)
+	master.address = address
+	master.workers = make(map[int]*RemoteWorker, 0)
+	master.idleWorkerChan = make(chan *RemoteWorker, IDLE_WORKER_BUFFER)
+	master.failedWorkerChan = make(chan *RemoteWorker, IDLE_WORKER_BUFFER)
+	master.failedOperation = make(chan bool, 1)
+	master.totalWorkers = 0
+	return
+}
+
+// acceptMultipleConnections will handle the connections from multiple workers.
+func (master *Master) acceptMultipleConnections() {
+	var (
+		err     error
+		newConn net.Conn
+	)
+
+	log.Printf("Accepting connections on %v\n", master.listener.Addr())
+
+	for {
+		newConn, err = master.listener.Accept()
+
+		if err == nil {
+			go master.handleConnection(&newConn)
+		} else {
+			log.Println("Failed to accept connection. Error: ", err)
+			break
+		}
+	}
+
+	log.Println("Stopped accepting connections.")
+}
+
+// handleFailingWorkers will handle workers that fails during an operation.
+func (master *Master) handleFailingWorkers() {
+	/////////////////////////
+	// YOUR CODE GOES HERE //
+	/////////////////////////
+
+	for failingWorker := range master.failedWorkerChan {
+		master.workersMutex.Lock()
+		log.Printf("Removing worker '%v' from master list", failingWorker.id)
+		delete(master.workers, failingWorker.id)
+		master.workersMutex.Unlock()
+	}
+	
+}
+
+// Handle a single connection until it's done, then closes it.
+func (master *Master) handleConnection(conn *net.Conn) error {
+	master.rpcServer.ServeConn(*conn)
+	(*conn).Close()
+	return nil
+}
